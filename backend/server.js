095c85c99b7506efa72a7f44d896599e54cb9768
dmmalawliya <dmmalawliya@gmail.com>
2024-11-10 17:12:41 +0530

    routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,12 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
+
+import cookieParser from "cookie-parser";
+
+import userRoutes from "./routes/user.routes.js"
+
+import messageRoutes from "./routes/message.route.js";
 import { connect } from "mongoose";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
@@ -14,11 +20,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 
 app.use("/api/auth",authRoutes);
 
+app.use("/api/messages",messageRoutes);
 
+app.use("/api/users",userRoutes);
 
 
 // app.get("/",(req, res) => {
